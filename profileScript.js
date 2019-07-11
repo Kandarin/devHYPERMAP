65f620f98b703b89cecc24b628cd87c0559ca487
plscks <plscks@icloud.com>
2019-07-11 15:18:17 -0500

    Undid stuff

--- profileScript.js
+++ profileScript.js
@@ -15,22 +15,5 @@ function lookup(name) {
   alert(name);
   var requestUrl = "https://www.nexusclash.com/modules.php?name=Character&charname=" + name + "&format=json";
 
-  $.ajaxSetup({
-		crossOrigin: true
-	});
-
-  $.ajax({
-    crossOrigin: true,
-    url: requestUrl,
-    proxy: "../proxy.php", //use local proxy because Google limits the use of Google Apps Script
-    //dataType: "json", //no need. if you use crossOrigin, the dataType will be override with "json"
-    //charset: 'ISO-8859-1', //use it to define the charset of the target url
-    context: {},
-    success: function(data) {
-      alert(data);
-      $( '#test' ).html(data);
-    }
-  })
-
   alert("STOP!")
 }
